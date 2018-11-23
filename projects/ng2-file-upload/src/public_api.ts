94e3339bbc598f3cedcfb2dbc2fd8237584e1c40
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-11-23 23:12:31 +0100

    NX workspace for app and library

--- projects/ng2-file-upload/src/public_api.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ng2-file-upload
- */
-
-export * from './lib/ng2-file-upload.service';
-export * from './lib/ng2-file-upload.component';
-export * from './lib/ng2-file-upload.module';
