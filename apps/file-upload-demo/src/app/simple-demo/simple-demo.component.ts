3a616baeb64f8e3c6d6da9fd2f1beef420fc0dc4
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-11-24 22:44:55 +0100

    Missing library usage in demo app

--- apps/file-upload-demo/src/app/simple-demo/simple-demo.component.ts
+++ apps/file-upload-demo/src/app/simple-demo/simple-demo.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FileUploader } from '@ng2/file-upload';
+
+// const URL = '/api/';
+const URL = 'https://evening-anchorage-3159.herokuapp.com/api/';
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -7,7 +11,41 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-demo.component.scss']
 })
 export class SimpleDemoComponent implements OnInit {
-  constructor() {}
+  uploader: FileUploader;
+  hasBaseDropZoneOver: boolean;
+  hasAnotherDropZoneOver: boolean;
+  response: string;
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.uploader = new FileUploader({
+      url: URL,
+      disableMultipart: true, // 'DisableMultipart' must be 'true' for formatDataFunction to be called.
+      formatDataFunctionIsAsync: true,
+      formatDataFunction: async item => {
+        return new Promise((resolve, reject) => {
+          resolve({
+            name: item._file.name,
+            length: item._file.size,
+            contentType: item._file.type,
+            date: new Date()
+          });
+        });
+      }
+    });
+
+    this.hasBaseDropZoneOver = false;
+    this.hasAnotherDropZoneOver = false;
+
+    this.response = '';
+
+    this.uploader.response.subscribe(res => (this.response = res));
+  }
+
+  fileOverBase(e: any): void {
+    this.hasBaseDropZoneOver = e;
+  }
+
+  fileOverAnother(e: any): void {
+    this.hasAnotherDropZoneOver = e;
+  }
 }
