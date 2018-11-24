e9349c39e67cf61fc446a02d039a69db3f66fcef
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-11-24 21:57:39 +0100

    Refine prettify usage

--- apps/file-upload-demo/src/app/app.component.ts
+++ apps/file-upload-demo/src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterContentInit, Component } from '@angular/core';
 
 import * as gettingStarted from 'html-loader!markdown-loader!../../src/docs/getting-started.md';
+
+declare var PR: any;
 
 @Component({
   // tslint:disable-next-line:component-selector
@@ -8,6 +10,15 @@ import * as gettingStarted from 'html-loader!markdown-loader!../../src/docs/gett
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterContentInit {
   gettingStarted: string = gettingStarted;
+
+  ngAfterContentInit() {
+    setTimeout(() => {
+      if (typeof PR !== 'undefined') {
+        // google code-prettify
+        PR.prettyPrint();
+      }
+    }, 150);
+  }
 }
