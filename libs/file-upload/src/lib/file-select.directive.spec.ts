db3e184f1c8080994cbad7ac4cf224e5b4381502
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-11-24 22:54:24 +0100

    Tests ported

--- libs/file-upload/src/lib/file-select.directive.spec.ts
+++ libs/file-upload/src/lib/file-select.directive.spec.ts
@@ -1,8 +1,109 @@
-import { FileSelectDirective } from './file-select.directive';
+import { Component, DebugElement } from '@angular/core';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
-describe('FileSelectDirective', () => {
-  it('should create an instance', () => {
-    const directive = new FileSelectDirective();
-    expect(directive).toBeTruthy();
+import { FileSelectDirective } from './file-select.directive';
+import { FileUploadModule } from './file-upload.module';
+import { FileUploader } from './file-uploader';
+
+@Component({
+  // tslint:disable-next-line:component-selector
+  selector: 'container',
+  template: `
+    <input type="file" ng2FileSelect [uploader]="uploader" />
+  `
+})
+export class ContainerComponent {
+  public get url(): string {
+    return 'localhost:3000';
+  }
+  public uploader: FileUploader = new FileUploader({ url: this.url });
+}
+
+describe('Directive: FileSelectDirective', () => {
+  let fixture: ComponentFixture<ContainerComponent>;
+  let hostComponent: ContainerComponent;
+  let directiveElement: DebugElement;
+  let fileSelectDirective: FileSelectDirective;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [FileUploadModule],
+      declarations: [ContainerComponent],
+      providers: [ContainerComponent]
+    });
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ContainerComponent);
+    hostComponent = fixture.componentInstance;
+
+    fixture.detectChanges();
+
+    directiveElement = fixture.debugElement.query(
+      By.directive(FileSelectDirective)
+    );
+    fileSelectDirective = directiveElement.injector.get(
+      FileSelectDirective
+    ) as FileSelectDirective;
+  });
+
+  it('can be initialized', () => {
+    expect(fixture).toBeDefined();
+    expect(hostComponent).toBeDefined();
+    expect(fileSelectDirective).toBeDefined();
+  });
+
+  it('can set file uploader', () => {
+    expect(fileSelectDirective.uploader).toBe(hostComponent.uploader);
+  });
+
+  it('can get uploader options', () => {
+    const options = fileSelectDirective.getOptions();
+
+    // Check url set through binding
+    expect(options.url).toBe(hostComponent.url);
+
+    // Check default options
+    expect(options.autoUpload).toBeFalsy();
+    expect(options.isHTML5).toBeTruthy();
+    expect(options.removeAfterUpload).toBeFalsy();
+    expect(options.disableMultipart).toBeFalsy();
+  });
+
+  it('can get filters', () => {
+    const filters = fileSelectDirective.getFilters();
+
+    // TODO: Update test once implemented
+    expect(filters).toEqual({});
+  });
+
+  it('can check if element is empty', () => {
+    const isElementEmpty = fileSelectDirective.isEmptyAfterSelection();
+
+    expect(isElementEmpty).toBeFalsy();
+  });
+
+  it('can listed on change event', () => {
+    spyOn(fileSelectDirective, 'onChange');
+
+    directiveElement.triggerEventHandler('change', {});
+
+    expect(fileSelectDirective.onChange).toHaveBeenCalled();
+  });
+
+  it('handles change event', () => {
+    spyOn(fileSelectDirective.uploader, 'addToQueue');
+
+    fileSelectDirective.onChange();
+
+    const expectedArguments = [
+      directiveElement.nativeElement.files,
+      fileSelectDirective.getOptions(),
+      fileSelectDirective.getFilters()
+    ];
+    expect(fileSelectDirective.uploader.addToQueue).toHaveBeenCalledWith(
+      ...expectedArguments
+    );
   });
 });
