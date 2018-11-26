b574dca1e9000755e6afe45e65691e4839e66561
Peter Blazejewicz <peter.blazejewicz@gmail.com>
2018-11-26 21:59:17 +0100

    Server app with multer: WIP

--- apps/file-upload-demo-server/src/main.ts
+++ apps/file-upload-demo-server/src/main.ts
@@ -3,8 +3,38 @@
  * This is only a minimal backend to get started.
  **/
 
-import * as express from 'express';
+import express from "express";
+import multer from 'multer';
 
+const storage = multer.diskStorage({
+  destination: (
+    req: Express.Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, folder: string) => void
+  ) => {
+    cb(null, './uploads/');
+  },
+  filename: (
+    req: Express.Request,
+    file: Express.Multer.File,
+    cb: (error: Error | null, filename: string) => void
+  ) => {
+    cb(null, new Date().toISOString());
+  }
+});
+
+const fileFilter = (
+  req: Express.Request,
+  file: Express.Multer.File,
+  cb: (error: Error | null, supportsMimeType: boolean) => void
+) => {
+  const mimeTypes = ['image/jpeg', 'image/png'];
+  if (mimeTypes.includes(file.mimetype.toLowerCase())) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 const app = express();
 
 app.get('/', (req, res) => {
@@ -12,7 +42,7 @@ app.get('/', (req, res) => {
 });
 
 const port = 3333;
-app.listen(port, (err) => {
+app.listen(port, err => {
   if (err) {
     console.error(err);
   }
